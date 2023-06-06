d10aa72066cc154b46e7ac322ccda5889915a3f3
Pavel Zh <pzworkmail@gmail.com>
2023-06-07 00:27:06 +0300

    initial commit

---
+++ FitNote/FitNote/FitNoteApp.swift
@@ -0,0 +1,17 @@
+//
+//  FitNoteApp.swift
+//  FitNote
+//
+//  Created by Pavel on 7.06.23.
+//
+
+import SwiftUI
+
+@main
+struct FitNoteApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
