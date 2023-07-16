c66d165842dc7451f64b5082efcb18faa3a5105e
Pavel Zh <pzworkmail@gmail.com>
2023-07-16 14:12:26 +0300

    text if no clients today

--- FitNote/FitNote/Trainer screen/TrainerView.swift
+++ FitNote/FitNote/Trainer screen/TrainerView.swift
@@ -165,75 +165,80 @@ struct TrainerView: View {
                                 Spacer()
                             }
                             .padding([.top, .bottom], 20)
-                            
-                            ScrollView {
-                                
-                                VStack {
+                            if vm.todayClients.isEmpty {
+                                Text("There are no clients today")
+                                    .foregroundColor(.red)
+                                    .font(.title2.bold())
+                            } else {
+                                ScrollView {
                                     
-                                    ForEach(vm.todayClients) { client in
+                                    VStack {
                                         
-                                        HStack {
+                                        ForEach(vm.todayClients) { client in
                                             
-                                            Text(client.time.stringTime())
-                                                .font(.title3.bold())
-                                                .foregroundColor(.darkColor)
-                                                .padding(10)
-                                                .background(Color.greenColor)
-                                                .padding(.leading, 20)
-                                            
-                                            VStack(alignment: .leading) {
+                                            HStack {
                                                 
-                                                Text(client.client.name)
-                                                    .foregroundColor(.white)
+                                                Text(client.time.stringTime())
                                                     .font(.title3.bold())
+                                                    .foregroundColor(.darkColor)
+                                                    .padding(10)
+                                                    .background(Color.greenColor)
+                                                    .padding(.leading, 20)
+                                                
+                                                VStack(alignment: .leading) {
+                                                    
+                                                    Text(client.client.name)
+                                                        .foregroundColor(.white)
+                                                        .font(.title3.bold())
+                                                    
+                                                    Spacer()
+                                                    
+                                                    HStack() {
+                                                        Text("Last workout:")
+                                                            .foregroundColor(.white)
+                                                        
+                                                        Text(vm.lastWorkouts[client.client.id] ?? "-")
+                                                            .foregroundColor(.greenColor)
+                                                            .padding(.leading, 10)
+                                                    }
+                                                    
+                                                }
+                                                .padding(.leading, 10)
                                                 
                                                 Spacer()
                                                 
-                                                HStack() {
-                                                    Text("Last workout:")
+                                                AsyncImage(url: URL(string: client.client.imageURL)) { image in
+                                                    image
+                                                        .resizable()
+                                                        .scaledToFill()
+                                                        .frame(width: 50, height: 50)
+                                                        .foregroundColor(.white)
+                                                        .overlay(Circle()
+                                                            .stroke(Color.greenColor, lineWidth: 2))
+                                                        .clipShape(Circle())
+                                                    
+                                                    
+                                                } placeholder: {
+                                                    Image("user")
+                                                        .renderingMode(.template)
+                                                        .resizable()
+                                                        .scaledToFill()
+                                                        .frame(width: 50, height: 50)
+                                                    
                                                         .foregroundColor(.white)
                                                     
-                                                    Text(vm.lastWorkouts[client.client.id] ?? "-")
-                                                        .foregroundColor(.greenColor)
-                                                        .padding(.leading, 10)
+                                                        .overlay(Circle()
+                                                            .stroke(Color.greenColor, lineWidth: 2))
+                                                        .clipShape(Circle())
                                                 }
-                                                
+                                                .padding(.trailing, 20)
                                             }
-                                            .padding(.leading, 10)
                                             
-                                            Spacer()
                                             
-                                            AsyncImage(url: URL(string: client.client.imageURL)) { image in
-                                                image
-                                                    .resizable()
-                                                    .scaledToFill()
-                                                    .frame(width: 50, height: 50)
-                                                    .foregroundColor(.white)
-                                                    .overlay(Circle()
-                                                        .stroke(Color.greenColor, lineWidth: 2))
-                                                    .clipShape(Circle())
-                                                
-                                                
-                                            } placeholder: {
-                                                Image("user")
-                                                    .renderingMode(.template)
-                                                    .resizable()
-                                                    .scaledToFill()
-                                                    .frame(width: 50, height: 50)
-                                                
-                                                    .foregroundColor(.white)
-                                                
-                                                    .overlay(Circle()
-                                                        .stroke(Color.greenColor, lineWidth: 2))
-                                                    .clipShape(Circle())
-                                            }
-                                            .padding(.trailing, 20)
+                                            Divider()
+                                                .background(.white)
+                                                .padding(.horizontal, 20)
                                         }
-                                        
-                                        
-                                        Divider()
-                                            .background(.white)
-                                            .padding(.horizontal, 20)
                                     }
                                 }
                             }
